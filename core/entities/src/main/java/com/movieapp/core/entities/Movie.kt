1d66047d6aeddee8bdbf1f0274a003027c7557f8
MoatazBadawy <moataz.egy101@gmail.com>
2023-03-26 22:28:52 +0200

    init entities

---
+++ core/entities/src/main/java/com/movieapp/core/entities/Movie.kt
@@ -0,0 +1,11 @@
+package com.movieapp.core.entities
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val posterPath: String,
+    val backdropPath: String,
+    val voteAverage: Double,
+    val releaseDate: String,
+)
