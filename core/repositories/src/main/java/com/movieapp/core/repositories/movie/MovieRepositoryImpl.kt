dbff868f7b042b4c9b517d63d13f8b54bdf3f0aa
MoatazBadawy <moataz.egy101@gmail.com>
2023-03-27 01:41:22 +0200

    create repository

---
+++ core/repositories/src/main/java/com/movieapp/core/repositories/movie/MovieRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.movieapp.core.repositories.movie
+
+import com.movieapp.core.entities.Movie
+import com.movieapp.core.repositories.mapper.toMovieList
+import com.movieapp.core.usecases.repository.MovieRepository
+import javax.inject.Inject
+
+class MovieRepositoryImpl @Inject constructor(
+    private val movieDataSource: MovieDataSource,
+) : MovieRepository {
+
+    override suspend fun getMostPopularMovies(): List<Movie> {
+        return movieDataSource.getMostPopularMovies().toMovieList()
+    }
+
+    override suspend fun getTopRatedMovies(): List<Movie> {
+        return movieDataSource.getTopRatedMovies().toMovieList()
+    }
+}
---
+++ core/usecases/src/main/java/com/movieapp/core/usecases/repository/MovieRepository.kt
@@ -0,0 +1,8 @@
+package com.movieapp.core.usecases.repository
+
+import com.movieapp.core.entities.Movie
+
+interface MovieRepository {
+    suspend fun getMostPopularMovies(): List<Movie>
+    suspend fun getTopRatedMovies(): List<Movie>
+}
