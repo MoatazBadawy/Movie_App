c32ded971518b0ed423c5c89e467b75720a5f7cc
MoatazBadawy <moataz.egy101@gmail.com>
2023-03-29 00:25:30 +0200

    create main screen and add App Icon

---
+++ app/src/main/java/com/moataz/movieapp/ui/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package com.moataz.movieapp.ui.di
+
+import com.moataz.movieapp.BuildConfig
+import com.movieapp.core.remote.AuthInterceptor
+import com.movieapp.core.remote.MovieService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.JavaNetCookieJar
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.net.CookieManager
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideMovieService(retrofit: Retrofit): MovieService {
+        return retrofit.create(MovieService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient, gsonConverterFactory: GsonConverterFactory): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(client)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(25, TimeUnit.SECONDS)
+            .connectTimeout(25, TimeUnit.SECONDS)
+            .cookieJar(JavaNetCookieJar(CookieManager()))
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+}
